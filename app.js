db529cacdb1d8ca71b21803af5b2abac24d5e2a4
Xavier Zapata <xjzapata12@gmail.com>
2020-05-26 16:28:29 -0700

    add mongoLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v13", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+// mongoose.connect("mongodb://localhost/yelp_camp_v13", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+//mongodb+srv://xjzapata:Sole!234@cluster0-betjc.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://xjzapata:Sole!234@cluster0-betjc.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
